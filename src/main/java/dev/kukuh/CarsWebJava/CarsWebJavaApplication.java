5cc7d54a950fded2de4155bc125b90ecacc72cde
kukuhsain <kukuhsain@gmail.com>
2024-12-26 05:27:48 +0700

    initiate project

---
+++ src/main/java/dev/kukuh/CarsWebJava/CarsWebJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.kukuh.CarsWebJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsWebJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsWebJavaApplication.class, args);
+	}
+
+}
