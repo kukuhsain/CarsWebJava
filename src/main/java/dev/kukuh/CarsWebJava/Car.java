ca6424326080b81ccf838f4da68f50c6478a4981
kukuhsain <kukuhsain@gmail.com>
2024-12-26 11:36:04 +0700

    enable to download results as XML

--- src/main/java/dev/kukuh/CarsWebJava/Car.java
+++ src/main/java/dev/kukuh/CarsWebJava/Car.java
@@ -1,11 +1,26 @@
 package dev.kukuh.CarsWebJava;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Car {
-    private long id;
-    private double length;
-    private double weight;
-    private double velocity;
-    private String color;
+    @XmlElement
+    public long id;
+    @XmlElement
+    public double length;
+    @XmlElement
+    public double weight;
+    @XmlElement
+    public double velocity;
+    @XmlElement
+    public String color;
+
+    public Car() {
+    }
 
     public Car(long id, double length, double weight, double velocity, String color) {
         this.id = id;
@@ -15,22 +30,27 @@ public class Car {
         this.color = color;
     }
 
+    // @XmlElement
     public long getId() {
         return id;
     }
 
+    // @XmlElement
     public double getLength() {
         return length;
     }
 
+    // @XmlElement
     public double getWeight() {
         return weight;
     }
 
+    // @XmlElement
     public double getVelocity() {
         return velocity;
     }
 
+    // @XmlElement
     public String getColor() {
         return color;
     }
--- src/main/java/dev/kukuh/CarsWebJava/CarController.java
+++ src/main/java/dev/kukuh/CarsWebJava/CarController.java
@@ -1,5 +1,6 @@
 package dev.kukuh.CarsWebJava;
 
+import java.io.OutputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.Marshaller;
 
 @Controller
 public class CarController {
@@ -32,7 +37,51 @@ public class CarController {
         model.addAttribute("weight", weight);
         model.addAttribute("velocity", velocity);
         model.addAttribute("color", color);
-        
+
         return "index";
+    }
+
+    @GetMapping(value = "/download")
+    public void downloadCarsAsXml(
+            @RequestParam(required = false) Double length,
+            @RequestParam(required = false) Double weight,
+            @RequestParam(required = false) Double velocity,
+            @RequestParam(required = false) String color,
+            HttpServletResponse response) {
+
+        // System.out
+        //         .println("length: " + length + ", weight: " + weight + ", velocity: " + velocity + ", color: " + color);
+        // List<Car> cars = carService.searchCars(length, weight, velocity, color);
+        List<Car> cars = carRepository.searchCars(length, weight, velocity, color);
+        System.out.println("cars: " + cars);
+        response.setContentType("application/xml");
+        response.setHeader("Content-Disposition", "attachment; filename=cars.xml");
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(Car.class);
+            // System.out.println("context: " + context);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            // System.out.println("marshaller: " + marshaller);
+            // StringWriter writer = new StringWriter();
+            // for (Car car : cars) {
+            // marshaller.marshal(car, writer);
+            // }
+            // System.out.println("writer: " + writer.toString());
+
+            // Write XML to response output stream
+            OutputStream outputStream = response.getOutputStream();
+            for (Car car : cars) {
+                // marshaller.marshal(car, writer);
+                marshaller.marshal(car, outputStream);
+            }
+            outputStream.flush();
+
+            // return writer.toString();
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting to XML", e);
+        }
+
+        // return carService.convertCarsToXml(cars);
     }
 }
